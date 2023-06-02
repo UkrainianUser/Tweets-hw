44ce545bf74c06f7b432660a2e6d15dda5a9cdc0
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-06-03 01:08:56 +0300

    remove test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from 'logo.svg';
+import css from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={css.app}>
+      <header className={css.header}>
+        <img src={logo} className={css.logo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={css.link}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
