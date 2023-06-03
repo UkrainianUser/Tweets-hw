ced8bc0a591d107a3086803f9bedbf27ad7ebbdb
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-06-03 16:38:01 +0300

    add button style

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,8 +4,10 @@ import css from './Header.module.css';
 const Header = () => {
   return (
     <header className={css.header}>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/tweets">Tweets</NavLink>
+      <div className="container">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/tweets">Tweets</NavLink>
+      </div>
     </header>
   );
 };
